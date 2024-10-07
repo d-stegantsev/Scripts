cbe72f1ef65930301517defe0798d5ba7c1b519f
d-stegantsev <stegan296@gmail.com>
2024-10-07 20:39:25 +0300

    Create Tab_employees_history.sql

---
+++ Tab_employees_history.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employees_history (employee_id NUMBER,
+                                first_name VARCHAR2(50),
+                                last_name VARCHAR2(50),
+                                job_id VARCHAR2(30),
+                                department_id NUMBER,
+                                hire_date DATE,
+                                termination_date DATE);
